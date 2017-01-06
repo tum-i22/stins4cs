f05f2b92656acac6b78b92dbdb7cf151ddda0c42
Ilya Migal <ilya.migal@hotmail.com>
2017-01-06 14:03:49 +0300

    Types are extracted  from Transform to a separate class

---
+++ transformer/Types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRoslynAnalysis
+{
+    class Types
+    {
+        // this will be the index of where not to continue using the first option
+        public static List<string> primitiveTypes = new List<string>()
+        {"byte","sbyte","int","uint","short","ushort","long","ulong","float","double","char","bool","string","decimal"};
+        public static List<string> numericPrimitiveTypes = new List<string>()
+        {"byte","sbyte","int","uint","short","ushort","long","ulong","float","double","decimal"};
+
+        public static bool IsPrimitive(String type)
+        {
+            return (primitiveTypes.Contains(type.Trim().ToLower()));
+        }
+        public static bool IsNumeric(string parentReturntype)
+        {
+            return (numericPrimitiveTypes.Contains(parentReturntype.Trim().ToLower()));
+
+        }
+
+    }
+}
